bf2bb3800a09cf9e03551d3bb1de4e3797c920a3
Savvypool <savvy.socialapp@gmail.com>
2023-02-14 17:13:10 +0530

    Savvypool

---
+++ js/main.js
@@ -0,0 +1,245 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').css('top', '0px');
+        } else {
+            $('.sticky-top').css('top', '-100px');
+        }
+    });
+    
+    
+    // Dropdown on mouse hover
+    const $dropdown = $(".dropdown");
+    const $dropdownToggle = $(".dropdown-toggle");
+    const $dropdownMenu = $(".dropdown-menu");
+    const showClass = "show";
+    
+    $(window).on("load resize", function() {
+        if (this.matchMedia("(min-width: 992px)").matches) {
+            $dropdown.hover(
+            function() {
+                const $this = $(this);
+                $this.addClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "true");
+                $this.find($dropdownMenu).addClass(showClass);
+            },
+            function() {
+                const $this = $(this);
+                $this.removeClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "false");
+                $this.find($dropdownMenu).removeClass(showClass);
+            }
+            );
+        } else {
+            $dropdown.off("mouseenter mouseleave");
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 24,
+        dots: true,
+        loop: true,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+})(jQuery);
+$(".card").click(function () {
+    $(".card").removeClass("active");
+    $(this).addClass("active");
+  });
+//faqs
+var acc = document.getElementsByClassName("accordion");
+var i;
+var len = acc.length;
+for (i = 0; i < len; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+let li = document.querySelectorAll(".faq-text li");
+for (var i = 0; i < li.length; i++) {
+  li[i].addEventListener("click", (e)=>{
+    let clickedLi;
+    if(e.target.classList.contains("question-arrow")){
+      clickedLi = e.target.parentElement;
+    }else{
+      clickedLi = e.target.parentElement.parentElement;
+    }
+   clickedLi.classList.toggle("showAnswer");
+  });
+}
+const jobs = [
+    {
+      title: "Senior Flutter Developer",
+      image: "img/software-engineer.svg",
+      details:
+        "2 - 4 years of experience in developing and delivering Flutter-based apps.",
+      openPositions: "1",
+      link: "job-detail.html",
+    },
+
+
+    {
+      title: "Junior Flutter Developer",
+      image: "img/software-engineer.svg",
+      details:
+        "1 - 2 years of experience in developing and delivering Flutter-based apps.",
+      openPositions: "1",
+      link: "job-detail jrF.html",
+    },
+
+
+    {
+      title: "Junior GoLang Developer",
+      image: "img/software-engineer.svg",
+      details:
+        "Good understanding of GoLang core,internals.Good exposure to Go concurrency",
+      openPositions: "1",
+      link: "job-detail jrGoLang.html",
+    },
+
+
+    {
+      title: "Senior GoLang Developer",
+      image: "img/software-engineer.svg",
+      details:
+      "Good understanding of GoLang core,internals.Good exposure to Go concurrency",
+      openPositions: "1",
+      link: "job-detail GoLang.html",
+      
+    },
+  ];
+  
+  const jobsHeading = document.querySelector(".jobs-list-container h2");
+  const jobsContainer = document.querySelector(".jobs-list-container .jobs");
+  const jobSearch = document.querySelector(".jobs-list-container .job-search");
+  
+  let searchTerm = "";
+  
+  if (jobs.length == 1) {
+    jobsHeading.innerHTML = `${jobs.length} Job`;
+  } else {
+    jobsHeading.innerHTML = `${jobs.length} Jobs`;
+  }
+  
+  const createJobListingCards = () => {
+    jobsContainer.innerHTML = "";
+  
+    jobs.forEach((job) => {
+      if (job.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+        let jobCard = document.createElement("div");
+        jobCard.classList.add("job");
+  
+        let image = document.createElement("img");
+        image.src = job.image;
+  
+        let title = document.createElement("h3");
+        title.innerHTML = job.title;
+        title.classList.add("job-title");
+  
+        let details = document.createElement("div");
+        details.innerHTML = job.details;
+        details.classList.add("details");
+  
+        let detailsBtn = document.createElement("a");
+        detailsBtn.href = job.link;
+        detailsBtn.innerHTML = "More Details";
+        detailsBtn.classList.add("details-btn");
+  
+        let openPositions = document.createElement("span");
+        openPositions.classList.add("open-positions");
+  
+        if (job.openPositions == 1) {
+          openPositions.innerHTML = `${job.openPositions} open position`;
+        } else {
+          openPositions.innerHTML = `${job.openPositions} open positions`;
+        }
+  
+        jobCard.appendChild(image);
+        jobCard.appendChild(title);
+        jobCard.appendChild(details);
+        jobCard.appendChild(detailsBtn);
+        jobCard.appendChild(openPositions);
+        
+  
+        jobsContainer.appendChild(jobCard);
+      }
+    });
+  };
+  
+  createJobListingCards();
+  
+  jobSearch.addEventListener("input", (e) => {
+    searchTerm = e.target.value;
+  
+    createJobListingCards();
+  });
